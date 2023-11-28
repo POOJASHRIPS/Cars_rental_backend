8082e1a4c8a3e6e4b608038a9960082902ee038f
Ebenesh <108049168+ebeneshcalv@users.noreply.github.com>
2023-11-28 12:54:52 +0530

    Delete car rental project- FE rework directory

--- car rental project- FE rework/forgotpassword.js
+++
@@ -1,9 +0,0 @@
-function callalert() {
-    const mailID = document.getElementById("MailID").value;
-    if (mailID === "") {
-        alert("Fill in the Mail ID");
-    }
-    else {
-        alert("Mail sent successfully to " + mailID);
-    }
-}
